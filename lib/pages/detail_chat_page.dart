c6dac38be2046f04259e6de4a783d783b8b2af90
AlvinSanudharma <alvinsanudharma@gmail.com>
2023-10-01 15:14:38 +0800

    Detail Chat Page Bagian Input 2

---
+++ lib/pages/detail_chat_page.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_app/theme.dart';
+
+class DetailChatPage extends StatelessWidget {
+  const DetailChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return PreferredSize(
+        preferredSize: const Size.fromHeight(77),
+        child: AppBar(
+          backgroundColor: backgroundColor1,
+          centerTitle: false,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo_online.png',
+                width: 50,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Shoes Store',
+                    style: primaryTextStyle.copyWith(
+                        fontWeight: medium, fontSize: 14),
+                  ),
+                  Text(
+                    'Online',
+                    style: secondaryTextStyle.copyWith(
+                        fontWeight: light, fontSize: 14),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: backgroundColor5,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: primaryColor)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISION 2.0',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(fontWeight: medium),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22.2,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                        color: backgroundColor4,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                            hintText: "Type Message...",
+                            hintStyle: subtitleTextStyle),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      bottomNavigationBar: chatInput(),
+    );
+  }
+}
--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -6,51 +6,54 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 33),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/image_shop_logo.png',
-                width: 54,
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Shoe Store',
-                      style: primaryTextStyle.copyWith(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      'Good night, This item is on...',
-                      style: secondaryTextStyle.copyWith(fontWeight: light),
-                    )
-                  ],
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, '/detail-chat'),
+      child: Container(
+        margin: const EdgeInsets.only(top: 33),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/image_shop_logo.png',
+                  width: 54,
                 ),
-              ),
-              Text(
-                'Now',
-                style: secondaryTextStyle.copyWith(fontSize: 10),
-                overflow: TextOverflow.ellipsis,
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const Divider(
-            thickness: 1,
-            color: Color(0xff2B2939),
-          )
-        ],
+                const SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Shoe Store',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        'Good night, This item is on...',
+                        style: secondaryTextStyle.copyWith(fontWeight: light),
+                      )
+                    ],
+                  ),
+                ),
+                Text(
+                  'Now',
+                  style: secondaryTextStyle.copyWith(fontSize: 10),
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Divider(
+              thickness: 1,
+              color: Color(0xff2B2939),
+            )
+          ],
+        ),
       ),
     );
   }
